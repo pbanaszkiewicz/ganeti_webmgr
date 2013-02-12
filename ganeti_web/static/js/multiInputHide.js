bca819a4440b9b2616988477ac5a90cae4d21f93
Chance Zibolski <chance.zibolski@gmail.com>
2013-02-12 12:18:17 -0800

    Add Javascript to dynamically hide/show inputs
    Moved some inline CSS for the form wizard into a CSS file as well.
    
    refs 11955

--- ganeti_web/static/js/multiInputHide.js
+++ ganeti_web/static/js/multiInputHide.js
@@ -1,24 +1,52 @@
-function multi_input(selector) {
-    var $selector = $(selector);
+function multi_input(scope) {
+    // the group of elements we're going to manipulate.
+    var $scope = $(scope);
+    // hide all empty inputs that are not the first input
+    $scope.filter(function() {
+            return $(this).data('group') != "0";
+        }).filter(function() {
+            return !$(this).val();
+        }).parents('tr').hide();
+
+    // button elements
     var $add = $('<a class="icon add multi"></a>');
     var $del = $('<a class="icon delete multi"></a>');
 
-    var cursor;
+    function first_hidden() {
+        return $scope.parents('tr:hidden:first');
+    }
+    function last_visible() {
+        return $scope.parents('tr:visible:last');
+    }
+
+    function place_buttons(){
+        // Cache it so we dont ask for it twice
+        var visible = last_visible();
+        $add.appendTo( visible.children('td').first() );
+        $del.appendTo( visible.children('td').first() );
+    }
+
+    // Add click handlers to both buttons
     $add.click(function(e) {
         e.preventDefault();
-        if (typeof(cursor) === 'undefined') {
-            cursor = $(this).parents("tr").next();
-        } else {
-            cursor = cursor.next();
-        }
-        cursor.show();
+        first_hidden().show();
+        place_buttons();
     });
 
-    // hide all but the first inputs for group of multi_inputs
-    $selector.not('.first').parents("tr").hide();
-
-    $('.first', $selector).each(function(index) {
-        $(this).parent().append($add);
-        $(this).parent().append($del);
+    $del.click(function(e) {
+        e.preventDefault();
+        // Cache the last item so we dont recompute it multiple times
+        var last = last_visible();
+        var group_num = $('.multi:not(.icon)', last).data('group')
+        // Do not hide/remove the only/first group.
+        if (group_num == 0) { return; }
+        var group = $(".multi:not(.icon)[data-group="+group_num+"]")
+        group.each(function(index) {
+            $(this).val('').parents('tr').hide();
+        });
+        place_buttons();
     });
+
+    // Initialization!
+    place_buttons();
 }
